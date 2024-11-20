15d37503a0eb2162a628efa01080ca88f2cca938
Dmitry-Gavelya <dimagavelya7@gmail.com>
2024-11-20 23:06:00 +0300

    fix ParserByVadim

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,5 +1,13 @@
 package app;
 
+import tgBot.parser.Article;
+import tgBot.parser.ourParsers.ParserByVadim;
+
 public class Main {
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    ParserByVadim parser = new ParserByVadim();
+    for (Article i : parser.parseAllSite()) {
+      System.out.println(i.title);
+    }
+  }
 }
