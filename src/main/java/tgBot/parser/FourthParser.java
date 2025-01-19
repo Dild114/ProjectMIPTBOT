07c9d4f67e00b255f3766841f42ce742a0ece10e
Sosnin_vadim <sosnin.vd@phystech.edu>
2025-01-19 19:12:22 +0300

    add FourthParser

--- src/main/java/tgBot/parser/FourthParser.java
+++ src/main/java/tgBot/parser/FourthParser.java
@@ -1,21 +1,18 @@
 package tgBot.parser;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 
 /**
  * Парсер сайта: <a href="https://3dnews.ru/">3dnews</a>
  */
 @Slf4j
-public class NewsParser implements SiteParser {
+public class FourthParser implements SiteParser {
   private static final String site = "https://3dnews.ru/";
 
   @Override
@@ -36,14 +33,13 @@ public class NewsParser implements SiteParser {
     final List<Article> data = new ArrayList<>();
     try {
       var posts = document.select("div#news.content-block div.content-block-data.white");
-      System.out.println(posts.size());
       for (var post : posts) {
         String title = post.select("table.nomargins a").text();
         String link = site + post.select("table.nomargins a").attr("href");
         String text = post.select("div.teaser").text();
         String date = "None";
 
-        data.add(new Article(title, link, text.substring(0, text.length() - 13), date));
+        data.add(new Article(title, link, text, date));
       }
     } catch (Exception e) {
       log.error("Ошибка во время парсинга сайта: {}", url, e);
