c98c7e0ab321951abdf510acdbe176b575e84bac
Sosnin_vadim <sosnin.vd@phystech.edu>
2025-01-19 18:09:46 +0300

    fix ParserManager

--- src/main/java/tgBot/parser/ParserManager.java
+++ src/main/java/tgBot/parser/ParserManager.java
@@ -1,5 +1,8 @@
 package tgBot.parser;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,18 +10,22 @@ public class ParserManager {
   private static final SiteParser HABRPARSER = new FirstParser();
   private static final SiteParser COMMUNITYPARSER = new SecondParser();
   private static final SiteParser XAKERPARSER = new ThirdParser();
+  private static final Logger log = LogManager.getLogger(ParserManager.class);
 
   public static List<Article> Manager(String url) {
     List<Article> data = new ArrayList<>();
     switch (url) {
-      case ("https://habr.com/ru/news/"):
+      case "https://habr.com/ru/news/":
         data = HABRPARSER.parseAllSite();
         break;
-      case ("https://timeweb.com/ru/community/"):
+      case "https://timeweb.com/ru/community/":
         data = COMMUNITYPARSER.parseAllSite();
         break;
-      case ("https://xakep.ru/"):
+      case "https://xakep.ru/":
         data = XAKERPARSER.parseAllSite();
+        break;
+      default:
+        log.error("Невозможно найти новости с данного сайта: {}", url);
       }
     return data;
   }
