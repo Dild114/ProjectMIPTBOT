1f87fc5d3afe4f7178b26aae4f63d593de2f24a5
Ulyalike <ryzhenkova.ue@phystech.edu>
2025-01-19 23:06:58 +0300

    Create dbStub.java

---
+++ src/main/java/app/api /dbStub.java
@@ -0,0 +1,142 @@
+package app.api;
+
+import app.api.entity.Article;
+import app.api.entity.ArticleId;
+import app.api.entity.Category;
+import app.api.entity.CategoryId;
+import app.api.entity.Site;
+import app.api.entity.SiteId;
+import app.api.entity.User;
+import app.api.entity.UserId;
+import app.api.repository.ArticleRepository;
+import app.api.repository.CategoryRepository;
+import app.api.repository.SiteRepository;
+import app.api.repository.UserRepository;
+import app.api.repository.dbArticleRepository;
+import app.api.repository.dbCategoryRepository;
+import app.api.repository.dbSiteRepository;
+import app.api.repository.MockUserRepository; 
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class dbStub implements ArticleRepository, CategoryRepository, SiteRepository, UserRepository {
+    private List<Article> articles = new ArrayList<>();
+    private List<Category> categories = new ArrayList<>();
+    private final List<Site> sites = new ArrayList<>();
+    private final List<User> users = new ArrayList<>();
+    private int articleIdCounter = 1;
+    private int categoryIdCounter = 1;
+    private int siteIdCounter = 1;
+    private int userIdCounter = 1;
+
+    @Override
+    public ArticleId generateIdArticle() {
+        return new ArticleId(articleIdCounter++);
+    }
+
+    @Override
+    public List<Article> getArticles(UserId userId) {
+        return new ArrayList<>(articles);
+    }
+
+    @Override
+    public CategoryId generateIdCategory() {
+        return new CategoryId(categoryIdCounter++);
+    }
+
+    @Override
+    public List<Category> findAllCategory(UserId userId) {
+        List<Category> answerCategory = new ArrayList<>();
+        for (Category category : categories) {
+            if (category.userId().equals(userId)) {
+                answerCategory.add(category);
+            }
+        }
+        return answerCategory;
+    }
+
+    @Override
+    public Category findCategoryById(CategoryId id) {
+        return categories.stream()
+                .filter(category -> category.id().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+    @Override
+    public boolean addCategory(Category category) {
+        return categories.add(category);
+    }
+
+    @Override
+    public boolean deleteCategory(CategoryId id, UserId userId) {
+        for (Category category : categories) {
+            if (category.userId().equals(userId)) {
+                categories.remove(category);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public SiteId generateIdSite() {
+        return new SiteId(siteIdCounter++);
+    }
+
+    @Override
+    public void deleteSiteById(SiteId id, UserId userId) {
+        sites.removeIf(site -> site.id().equals(id) && site.userId().equals(userId));
+    }
+
+    @Override
+    public void addSite(Site site) {
+        sites.add(site);
+    }
+
+    @Override
+    public List<Site> findAllSite(UserId userId) {
+        List<Site> answerSites = new ArrayList<>();
+        for (Site site : sites) {
+            if (site.userId().equals(userId)) {
+                answerSites.add(site);
+            }
+        }
+        return answerSites;
+    }
+
+    @Override
+    public Site findSiteById(SiteId id) {
+        return sites.stream()
+                .filter(site -> site.id().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+    @Override
+    public UserId generateUserId() {
+        return new UserId(userIdCounter++);
+    }
+
+    @Override
+    public boolean createUser(User user) {
+        return users.add(user);
+    }
+
+    @Override
+    public boolean deleteUser(UserId userId) {
+        for (Category category : categories) {
+            if (category.userId().equals(userId)) {
+                categories.remove(category);
+            }
+        }
+
+        for (Site site : sites) {
+            if (site.userId().equals(userId)) {
+                sites.remove(site);
+            }
+        }
+        return users.removeIf(user -> user.telegramId().equals(userId));
+    }
+}
