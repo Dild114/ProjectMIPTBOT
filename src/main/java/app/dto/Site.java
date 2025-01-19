aa0fe183d3060d0111298756a71934cb1cce9379
Ulyalike <ryzhenkova.ue@phystech.edu>
2025-01-19 21:02:48 +0300

    Create Site.java

---
+++ src/main/java/app/dto/Site.java
@@ -0,0 +1,13 @@
+package app.dto;
+
+public class Site {
+    private final String url;
+
+    public Site(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+}
